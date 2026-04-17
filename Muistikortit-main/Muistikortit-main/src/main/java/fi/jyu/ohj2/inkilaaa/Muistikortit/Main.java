a1bcb8cc35e6c96017cd2c17a851cf789c22c8b3
inkilaaa <aapo.inkila@gmail.com>
2026-04-17 10:30:00 +0300

    Delete Muistikortit-main/Muistikortit-main directory

--- Muistikortit-main/Muistikortit-main/src/main/java/fi/jyu/ohj2/inkilaaa/Muistikortit/Main.java
+++
@@ -1,9 +0,0 @@
-package fi.jyu.ohj2.inkilaaa.Muistikortit;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
